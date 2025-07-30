9debdf174d0e4004d4ad5a8764cbf720d804e2ee
hanwenli <hanwenli@amazon.com>
2025-07-30 08:15:38 -0700

    Upgrade dependencies
    - Upgrade Slurm to version 24.11.6 (from 24.05.8).
    - Upgrade EFA installer to 1.42.0 (from 1.41.0).
      - Efa-driver: efa-2.15.3-1
      - Efa-config: efa-config-1.18-1
      - Efa-profile: efa-profile-1.7-1
      - Libfabric-aws: libfabric-aws-2.1.0-3
      - Rdma-core: rdma-core-57.0-1
      - Open MPI: openmpi40-aws-4.1.7-2 and openmpi50-aws-5.0.6-11
    - Upgrade Cinc Client to version to 18.4.12 from 18.2.7.
    - Upgrade NVIDIA driver to version 570.172.08 (from 570.86.15) for all OSs except AL2.
    - Upgrade CUDA Toolkit to version 12.8.1 (from 12.8.0) for all OSs except AL2.
    - Upgrade DCGM to version 4.2.3 (from 3.3.6) for all OSs except AL2.
    - Upgrade Python to 3.12.11 (from 3.12.8) for all OSs except AL2.
    - Upgrade Intel MPI Library to 2021.16.0 (from 2021.13.1).
    
    Among the above upgrade, DCGM is a major version upgrade (from version 3 to version 4)
    This is a new change in DCGM 4:
    ```
    Installation assets are no longer shipped in a single monolithic package. Instead, installation assets have been split among several packages, allowing clients to opt-out of the installation of assets not applicable to their use case.
    
      Component packages are as follows:
    
          datacenter-gpu-manager-4-core
    
                  Provides nv-hostengine binary and other CUDA-agnostic installation assets available through the DCGM open source product
    
          datacenter-gpu-manager-4-cuda11
    
                  Provides the CUDA11-specific binaries available through the DCGM open source product
    
          datacenter-gpu-manager-4-cuda12
    
                  Provides the CUDA12-specific binaries available through the DCGM open source product
    
          datacenter-gpu-manager-4-proprietary
    
                  Provides CUDA-agnostic installation assets not distributed as part of the DCGM open source product
    
          datacenter-gpu-manager-4-proprietary-cuda11
    
                  Provides CUDA11 binaries not distributed as part of the DCGM open source product
    
          datacenter-gpu-manager-4-proprietary-cuda12
    
                  Provides CUDA12 binaries not distributed as part of the DCGM open source product
    
          datacenter-gpu-manager-4-development
    
                  Provides files necessary for the development of downstream software dependent on the DCGM library
    
    ```
    https://docs.nvidia.com/datacenter/dcgm/latest/release-notes/changelog.html
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cookbooks/aws-parallelcluster-platform/attributes/platform.rb
+++ cookbooks/aws-parallelcluster-platform/attributes/platform.rb
@@ -16,10 +16,11 @@ default['cluster']['enroot']['persistent_dir'] = '/var/enroot'
 
 # NVidia
 default['cluster']['nvidia']['enabled'] = 'no'
-default['cluster']['nvidia']['driver_version'] = '570.86.15'
-default['cluster']['nvidia']['dcgm_version'] = '3.3.6'
+default['cluster']['nvidia']['driver_version'] = '570.172.08'
+default['cluster']['nvidia']['dcgm_version'] = '4.2.3-2'
 if platform?('amazon') && node['platform_version'] == "2"
   default['cluster']['nvidia']['driver_version'] = '550.127.08'
+  default['cluster']['nvidia']['dcgm_version'] = '3.3.6-1'
 end
 
 # DCV
